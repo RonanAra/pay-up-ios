dfb63a20d13e68c2e5f53c710ad79cc91269b89a
Ronan Fernandes <ronanlm10@hotmail.com>
2026-07-18 13:18:59 -0300

    feat: Added base assets and extensions

---
+++ PayUp/Sources/Core/Extensions/UIView+Ext.swift
@@ -0,0 +1,22 @@
+//
+//  UIView+Ext.swift
+//  PayUp
+//
+//  Created by Ronan Fernandes on 18/07/26.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    func animateMoveOut(to point: CGPoint, duration: TimeInterval) {
+        UIView.animate(
+            withDuration: duration,
+            delay: 0,
+            options: .curveEaseInOut
+        ) {
+            self.center = point
+            self.alpha = 0
+        }
+    }
+}
