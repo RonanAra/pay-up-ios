ca67b2219b73cc087522aeae90cede7df4f61a4b
Ronan Fernandes <ronanlm10@hotmail.com>
2026-09-03 17:11:27 -0300

    feat: add SplashView

--- PayUp/Sources/Features/Splash/ViewModel/SplashViewModel.swift
+++ PayUp/Sources/Features/Splash/ViewModel/SplashViewModel.swift
@@ -2,16 +2,15 @@
 //  SplashViewModel.swift
 //  PayUp
 //
-//  Created by Ronan Fernandes on 20/07/26.
+//  Created by Arthur Rios on 23/04/25.
 //
 
 import Foundation
 
 final class SplashViewModel {
-    
     var onAnimationCompleted: (() -> Void)?
     
-    func performAnimation(completion: @escaping () -> Void) {
+    func performInitialAnimation(completion: @escaping () -> Void) {
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
             completion()
         }
